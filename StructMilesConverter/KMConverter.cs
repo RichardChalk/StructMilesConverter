5bc85a509526b261302abbadb02a161d11da3ec3
Richard Chalk <barmybrit@gmail.com>
2023-06-30 14:21:00 +0200

    Add project files.

---
+++ StructMilesConverter/KMConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExample
+{
+    public struct KMConverter
+    {
+        private float km;
+        private float miles;
+
+        public KMConverter(float km)
+        {
+            this.km = km;
+        }
+        public float Km
+        {
+            get { return km; }
+            set { km = value; }
+        }
+
+        public float Miles
+        {
+            get { return miles; }
+            set { miles = value; }
+        }
+
+        public void CalculateMiles()
+        {
+            Miles = km * 1.6f;
+        }
+    }
+}
---
+++ StructMilesConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StructExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myKmStruct = new KMConverter(113);
+            myKmStruct.CalculateMiles();
+            
+            Console.WriteLine($"Total km: {myKmStruct.Km}");
+            Console.WriteLine($"Total miles: {myKmStruct.Miles}");
+        }
+    }
+}
